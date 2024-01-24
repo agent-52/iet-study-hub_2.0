693b608d35a881a4a40c82c0ea1697dde8ddfebe
agent-52 <abhaybhadauriya52@gmail.com>
2024-01-24 18:16:28 +0530

    add more subjects

--- script1.js
+++ script1.js
@@ -1,19 +1,34 @@
 const subjectslist1 = document.querySelector('.transparent-text');
 
-const mathHeading = document.querySelector('.light-white-subject');
+const mathHeading = document.querySelector('.math1');
 const mathImage = document.querySelector('.math-image');
 
-const emeHeading = document.querySelector('.white-subject');
+const emeHeading = document.querySelector('.eme');
 const emeImage = document.querySelector('.eme-image');
 
-const electronicsHeading = document.querySelector('.more-white-subject');
+const electronicsHeading = document.querySelector('.basicelectronics');
 const electronicsImage = document.querySelector('.electronics-image');
 
-const chemistryHeading = document.querySelector('.orange-subject');
+const chemistryHeading = document.querySelector('.chemistry');
 const chemistryImage = document.querySelector('.chemistry-image');
 
-const englishHeading = document.querySelector('.light-blue-subject');
+const englishHeading = document.querySelector('.english');
 const englishImage = document.querySelector('.english-image')
+
+const math2Heading = document.querySelector('.math2');
+const math2Image = document.querySelector('.math2-image');
+
+const edHeading = document.querySelector('.ed');
+const edImage = document.querySelector('.ed-image');
+
+const eeHeading = document.querySelector('.electrical-engineering');
+const eeImage = document.querySelector('.electrical-engineering-image');
+
+const physicsHeading = document.querySelector('.physics');
+const physicsImage = document.querySelector('.physics-image');
+
+const humanitiesHeading = document.querySelector('.humanities');
+const humanitiesImage = document.querySelector('.humanities-image');
 
 document.querySelector('.light-white-subject').addEventListener('mouseenter', () => {
   subjectslist1.classList.add("light-white-outline");
@@ -142,6 +157,81 @@ englishHeading.addEventListener('mouseenter', () => {
   emeImage.classList.add('hidden')
   mathImage.classList.remove('visible')
   mathImage.classList.add('hidden')
+  
+})
+
+
+
+
+
+math2Heading.addEventListener('mouseenter', () => {
+  math2Image.classList.remove('hidden')
+  math2Image.classList.add('visible')
+  physicsImage.classList.remove('visible')
+  physicsImage.classList.add('hidden')
+  eeImage.classList.remove('visible')
+  eeImage.classList.add('hidden')
+  edImage.classList.remove('visible')
+  edImage.classList.add('hidden')
+  humanitiesImage.classList.remove('visible')
+  humanitiesImage.classList.add('hidden')
+  
+ 
+})
+
+edHeading.addEventListener('mouseenter', () => {
+  edImage.classList.remove('hidden')
+  edImage.classList.add('visible')
+  physicsImage.classList.remove('visible')
+  physicsImage.classList.add('hidden')
+  eeImage.classList.remove('visible')
+  eeImage.classList.add('hidden')
+  math2Image.classList.remove('visible')
+  math2Image.classList.add('hidden')
+  humanitiesImage.classList.remove('visible')
+  humanitiesImage.classList.add('hidden')
+ 
+})
+
+eeHeading.addEventListener('mouseenter', () => {
+  eeImage.classList.remove('hidden')
+  eeImage.classList.add('visible')
+  physicsImage.classList.remove('visible')
+  physicsImage.classList.add('hidden')
+  edImage.classList.remove('visible')
+  edImage.classList.add('hidden')
+  math2Image.classList.remove('visible')
+  math2Image.classList.add('hidden')
+  humanitiesImage.classList.remove('visible')
+  humanitiesImage.classList.add('hidden')
+  
+})
+
+physicsHeading.addEventListener('mouseenter', () => {
+  physicsImage.classList.remove('hidden')
+  physicsImage.classList.add('visible')
+  eeImage.classList.remove('visible')
+  eeImage.classList.add('hidden')
+  edImage.classList.remove('visible')
+  edImage.classList.add('hidden')
+  math2Image.classList.remove('visible')
+  math2Image.classList.add('hidden')
+  humanitiesImage.classList.remove('visible')
+  humanitiesImage.classList.add('hidden')
+  
+})
+
+humanitiesHeading.addEventListener('mouseenter', () => {
+  humanitiesImage.classList.remove('hidden')
+  humanitiesImage.classList.add('visible')
+  physicsImage.classList.remove('visible')
+  physicsImage.classList.add('hidden')
+  eeImage.classList.remove('visible')
+  eeImage.classList.add('hidden')
+  edImage.classList.remove('visible')
+  edImage.classList.add('hidden')
+  math2Image.classList.remove('visible')
+  math2Image.classList.add('hidden')
   
 })
 
